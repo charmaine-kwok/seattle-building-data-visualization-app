5cf2f50ec206a2647c0b5d5657aeed2267858ca1
charmaine-kwok <mickeymousekwok97@yahoo.com.hk>
2023-06-12 05:37:22 +0800

    change login page styling

--- frontend/src/pages/login.tsx
+++ frontend/src/pages/login.tsx
@@ -56,25 +56,29 @@ const Login = () => {
 
   return (
     <div className="h-screen flex bg-gray-bg1">
-      <div className="w-full max-w-md m-auto bg-white rounded-lg border py-10 px-16">
+      <div className="w-full max-w-2xl m-auto bg-white rounded-lg border py-10 px-16">
         <h1 className="text-2xl font-medium text-primary mt-4 mb-12 text-center">
-          Log in to your account 🔐
+          SEATTLE BUILDING DATA VISUALIZATION
         </h1>
         <form>
-          <div>
-            <label htmlFor="username">Username</label>
+          <div className="flex-row flex items-center justify-center">
+            <label className="w-[20%]" htmlFor="username">
+              Username
+            </label>
             <input
               type="string"
-              className={`w-full p-2 border rounded-md outline-none text-sm mb-4`}
+              className={`w-[70%] p-2 border rounded-md outline-none text-sm ml-2`}
               placeholder="Your Username"
               ref={usernameRef}
             />
           </div>
-          <div>
-            <label htmlFor="password">Password</label>
+          <div className="my-2 flex-row flex items-center justify-center">
+            <label className="w-[20%]" htmlFor="password">
+              Password
+            </label>
             <input
               type="password"
-              className={`w-full p-2 border rounded-md outline-none text-sm  mb-4`}
+              className={`w-[70%] p-2 border rounded-md outline-none text-sm ml-2`}
               placeholder="Your Password"
               ref={passwordRef}
             />
@@ -82,9 +86,9 @@ const Login = () => {
           <div className="flex justify-center items-center mt-6">
             <button
               onClick={handleClick}
-              className={` bg-green-400 py-2 px-4 text-sm text-black rounded hover:bg-green-300`}
+              className={` bg-red-400 w-[30%] py-2 px-4 text-sm text-black border border-black border-w-1 rounded-md`}
             >
-              Login
+              LOGIN
             </button>
           </div>
         </form>
