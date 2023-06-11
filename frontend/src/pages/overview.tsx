642fbf1895424729711f4773376e83d64ee66885
charmaine-kwok <mickeymousekwok97@yahoo.com.hk>
2023-06-12 05:01:27 +0800

    add styling to overview page

--- frontend/src/pages/overview.tsx
+++ frontend/src/pages/overview.tsx
@@ -9,14 +9,16 @@ interface BuildingsProps {
 
 interface BuildingsResponse {
   buildings: BuildingsProps[];
+  totalItem: number;
 }
-
 export default function Overview() {
   const jwtAtomValue = useAtomValue(jwtAtom);
   const [buildingsList, setBuildingsList] = useState<BuildingsProps[]>([]);
-  console.log(jwtAtomValue);
+  const [page, setPage] = useState<number>(1);
+  const [totalItem, setTotalItem] = useState<number>(0);
+
   const fetchBuildings = () => {
-    fetch("http://localhost:8081/overview", {
+    fetch(`http://localhost:8081/overview?page=${page}`, {
       credentials: "include",
     })
       .then((response) => {
@@ -30,6 +32,8 @@ export default function Overview() {
       .then((data) => {
         console.log(data.buildings);
         setBuildingsList(data.buildings);
+        setTotalItem(data.totalItem);
+        setBuildingsList([...buildingsList, ...data.buildings]);
       })
       .catch((error) => {
         console.error(error);
@@ -37,18 +41,35 @@ export default function Overview() {
       });
   };
 
-  useEffect(() => fetchBuildings(), []);
+  useEffect(() => fetchBuildings(), [page]);
+
+  const handleMoreButtonClick = () => {
+    setPage(page + 1);
+  };
 
   return (
-    <div className="h-screen flex bg-gray-bg1">
-      <h1>Overview</h1>
-      <ul>
-        {buildingsList.map((building, index) => (
-          <li key={index}>
-            {building.City} - {building.PropertyName}
-          </li>
-        ))}
-      </ul>
+    <div className="flex flex-col items-center justify-center py-4">
+      <h1 className="text-3xl font-bold my-4">Overview</h1>
+      <div className="flex flex-col items-center justify-center space-y-4 w-[60%]">
+        <ul className="space-y-2 flex-col w-[100%]">
+          {buildingsList.map((building, index) => (
+            <li
+              key={index}
+              className="py-2 px-2 border border-w-[1px] border-black"
+            >
+              {building.City} - {building.PropertyName}
+            </li>
+          ))}
+        </ul>
+        {buildingsList.length < totalItem && (
+          <button
+            className="py-4 px-4 border border-w-[1px] border-green-400 bg-green-400 rounded-md"
+            onClick={handleMoreButtonClick}
+          >
+            More
+          </button>
+        )}
+      </div>
     </div>
   );
 }
